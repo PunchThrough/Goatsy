b61ce3ca711155ff2e824860dfcec5a1f6f13aa6
Alexia McKenzie <pandapprentice@gmail.com>
2014-10-17 19:28:30 -0700

    initial commit - working

---
+++ Readme.md
@@ -0,0 +1,4 @@
+<h1>Goatsy</h1>
+<br />
+This is a fun project that you can make with a <a href="http://www.punchthrough.com/bean/">Light Blue Bean!</a>
+Combined a few RGB LEDs and your favorite stuff goat, you too can have a fierce new friend!
---
+++ goatsy.ino
@@ -0,0 +1,49 @@
+int BlueRight = 0,
+    GreenRight = 1, //broken ##needs re-solder
+    RedRight = 2,
+    BlueLeft = 3,
+    GreenLeft = 4,
+    RedLeft = 5;
+
+void setup() {
+  pinMode(BlueRight, OUTPUT);
+  pinMode(GreenRight, OUTPUT); //broken
+  pinMode(RedRight, OUTPUT);
+  pinMode(BlueLeft, OUTPUT);
+  pinMode(GreenLeft, OUTPUT);
+  pinMode(RedLeft, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(RedRight, HIGH); // high is on
+  digitalWrite(RedLeft, HIGH);
+  delay(500);
+  digitalWrite(RedRight, LOW);
+  digitalWrite(RedLeft, LOW);
+  delay(50);
+  digitalWrite(RedRight, HIGH);
+  digitalWrite(RedLeft, HIGH);
+  delay(100);
+  digitalWrite(RedRight, LOW);
+  digitalWrite(RedLeft, LOW);
+  delay(200);
+  digitalWrite(RedRight, HIGH);
+  digitalWrite(RedLeft, HIGH);
+  delay(100);
+  digitalWrite(RedRight, LOW);
+  digitalWrite(RedLeft, LOW);
+  delay(200);
+  digitalWrite(RedRight, HIGH);
+  digitalWrite(RedLeft, HIGH);
+  delay(750);
+  digitalWrite(RedRight, HIGH);
+  digitalWrite(RedLeft, HIGH);
+  delay(100);
+  digitalWrite(RedRight, LOW);
+  digitalWrite(RedLeft, LOW);
+  delay(200);
+  digitalWrite(RedRight, LOW); //low is off
+  digitalWrite(RedLeft, LOW);
+  delay(500);
+  Bean.sleep(750);
+}
